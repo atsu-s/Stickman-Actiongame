185c506759d5492f315b0fe9a309e867c3c964a4
曽根惇史 <82425024+atsu-s@users.noreply.github.com>
2021-06-10 12:49:19 +0900

    player_move・GroundCheck

--- SticManActionGame/Assets/Scripts/player.cs
+++ SticManActionGame/Assets/Scripts/player.cs
@@ -4,31 +4,50 @@ using UnityEngine;
 
 public class player : MonoBehaviour
 {
+    // インスペクターで設定
+    public float speed;
+    public GroundCheck ground;
+
+    // プライベート変数
     private Animator anim = null;
+    private Rigidbody2D rb = null;
+    private bool isGround = false;
+
     // Start is called before the first frame update
     void Start()
     {
+        // コンポーネントのインスタンスを捕まえる
         anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        // 接地判定を受け取る
+        isGround = ground.IsGround();
+
+        // キー入力に関する処理
         float horizontalKey = Input.GetAxis("Horizontal");
+        float xSpeed = 0.0f;
 
         if(horizontalKey > 0)
         {
             transform.localScale = new Vector3(1, 1, 1);
             anim.SetBool("run", true);
+            xSpeed = speed;
         }
         else if(horizontalKey < 0)
         {
             transform.localScale = new Vector3(-1, 1, 1);
             anim.SetBool("run", true);
+            xSpeed = -speed;
         }
         else
         {
             anim.SetBool("run", false);
+            xSpeed = 0.0f;
         }
+        rb.velocity = new Vector2(xSpeed, rb.velocity.y);
     }
 }
